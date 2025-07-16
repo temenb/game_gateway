d40cc8f5732220904edc6b1f85d51021b30d1f93
Oleksii Temchenko <OTemchenko@fxcess.com>
2025-07-16 12:36:07 +0300

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts']
 };
