f772d40e1d16af4ea082cb90454116a8479aac9d
Oleksii Temchenko <OTemchenko@fxcess.com>
2025-08-11 16:11:23 +0300

    stable

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-app.post('/health', (_, res) => res.send('OK'));
+app.get('/health', (_, res) => res.send('OK'));
 
 app.post('/auth/register', authClient.register);
 app.post('/auth/login', authClient.login);
